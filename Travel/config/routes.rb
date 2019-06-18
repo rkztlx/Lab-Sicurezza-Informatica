c2d5c28c199dd7d5a767547178fc7eac8d8163f1
rkztlx <Valentin-m97@live.it>
2019-06-18 22:33:46 +0200

    new pages haml

--- Travel/config/routes.rb
+++ Travel/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'static_pages/home'
+  get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :places
   resources :users
   resources :reviews
-  root :to => redirect('/places')
+  root  'static_pages#home'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
