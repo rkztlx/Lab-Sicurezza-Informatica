7e159f7a59ba2be914a3b0a8d6b5f6c84aac8856
lorenzoproietti <lorenzoproietti16@outlook.it>
2019-06-25 19:37:23 +0200

    added set profile

--- Travel/app/controllers/application_controller.rb
+++ Travel/app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :name, :surname, :birth_date, :nickname, :bio)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :name, :surname, :birth_date, :nickname, :bio)}
   end
 
 end
