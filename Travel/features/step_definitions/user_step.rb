a616d0044057447c9637c71ba7d27271b7b12ccd
Matrix <Valentin-m97@live.it>
2019-06-30 16:37:08 +0200

    added 4 features for user

--- Travel/features/step_definitions/user_step.rb
+++ Travel/features/step_definitions/user_step.rb
@@ -2,35 +2,38 @@ Given("no user exists with an email of {string}") do |string|
     User.find_by_email(string).should be_nil
 end
 
-Given("An user is signed up and confirmed as {string}, {string}, {string}, {string}, {string}") do |email, password, name, surname, nickname|
-    User.create(email: email, password: password, password_confirmation: password, name: name, surname: surname, nickname: nickname)
-    click_button "Sign up"
+When("The user sign up as {string},{string},{string},{string},{string}") do |email, password, name, surname, nickname|
+   User.create(email: email, password: password, name: name, surname: surname, nickname: nickname)
+   click_button "Sign up"
 end
 
-Given("A registered user as {string}, {string}") do |email, password|
-    User.create(email: email, password: password, password_confirmation: password)
-    visit new_user_session_path
-    fill_in 'user_email', :with => email
-    fill_in 'user_password', :with => password
-    click_button 'Log in'
+Then("The user should be redirected to {string} page") do |page|
+    expect(current_path).to eql page
+end
+
+
+Given("A registered user as {string}") do |email|
+    User.find_by_email(email)
+end
+
+Given("An User is signed up and confirmed as {string},{string},{string},{string},{string}") do |email, password, name, surname, nickname|
+    User.create(email: email, password: password, name: name, surname: surname, nickname: nickname)
 end
 
 When("An user go to the {string} page") do |page|
     visit page
 end
 
-When("The user sign in as {string}, {string}") do |email, password|
+When("The user sign in as {string},{string}") do |email, password|
     fill_in 'user_email', :with => email
     fill_in 'user_password', :with => password
     click_button 'Log in'
 end
 
-
-Then("The user should be to redirect to {string}") do |page|
-    expect(current_path).to eql page
-end
-
 When("An user click the {string} button") do |button|
     click_button button
 end
-  
+
+Then("An user should see the {string} button") do |button|
+    expect(page).to have_content("Log in")
+end
--- Travel/features/user.feature
+++ Travel/features/user.feature
@@ -1,8 +1,24 @@
 Feature: USER features
   
   Scenario: As an UNREGISTERED USER I want to DISPLAY THE SIGN IN PAGE
-    Given no user exists with an email of "email@person.com"
+    Given no user exists with an email of "email@user.com"
     When An user go to the "/static_pages/home" page
     And An user click the "Search" button
-    Then The user should be to redirect to "/users/sign_in"
+    Then The user should be redirected to "/users/sign_in" page
 
+  Scenario: As an UNREGISTERED USER I want to SIGN UP WITH EMAIL
+    Given no user exists with an email of "email@user.com"
+    When An user go to the "/users/sign_up" page
+    And The user sign up as "email@user.com","password","pippo","franco","pippo1"
+    Then The user should be redirected to "/users" page
+    
+  Scenario: As a USER I want to SIGN IN WITH E-MAIL
+    Given An User is signed up and confirmed as "email@user.com","password","pippo","franco","pippo1"  
+    When An user go to the "/users/sign_in" page
+    And The user sign in as "email@user.com","password"
+    Then The user should be redirected to "/static_pages/home" page
+    
+  Scenario: As a USER I want to LOGOUT so I can LOG IN with another account
+    Given A registered user as "email@user.com"
+    When An user go to the "/users/sign_out" page
+    Then An user should see the "Log In" button
